2b2d9df811aa9a53a565e4a25ec520501fade280
julia-ctp <juliactp.santos@gmail.com>
2026-03-25 22:20:36 -0300

    feat: cors config

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from "express";
 import quizRoutes from "./routes/quiz.routes.js";
 import swaggerUi from "swagger-ui-express";
 import { swaggerConfig } from "./docs/swagger/swagger.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/health", (_req, res) => {
   res.json({ status: "ok", uptime: process.uptime() });
